66d33683e1466e629caeda8357330526007109b8
Mohab <82895440+Mohab-12@users.noreply.github.com>
2025-03-05 12:50:22 +0200

    Update Streamlit_widget.py

--- Streamlit_widget.py
+++ Streamlit_widget.py
@@ -631,10 +631,10 @@ fig1.update_layout(title='Experimental VS Calculated',
 
 # Create a line plot for temperature profiles
 fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=np.arange(1, 9, 1), y=Flue_gas[:-1], mode='markers', name='Experimental outlet air temperature'))
+fig2.add_trace(go.Scatter(x=np.linspace(1, n, 8), y=Flue_gas[:-1], mode='markers', name='Experimental outlet air temperature'))
 fig2.add_trace(go.Scatter(x=np.linspace(1, n, n), y=Outlet_temp_air, mode='lines', name='Calculated outlet air temperature', line=dict(color='red', dash='dash')))
 
-fig2.add_trace(go.Scatter(x=np.arange(1, 9, 1), y=Cooling_water[:-1], mode='markers', name='Experimental outlet water temperature',marker=dict(color='blue')))
+fig2.add_trace(go.Scatter(x=np.linspace(1, n, 8), y=Cooling_water[:-1], mode='markers', name='Experimental outlet water temperature',marker=dict(color='blue')))
 fig2.add_trace(go.Scatter(x=np.linspace(1, n, n), y=Inlet_temp_water, mode='lines', name='Calculated outlet water temperature', line=dict(color='red', dash='dash')))
 
 fig2.update_layout(title='Temperature profiles',
