65d4454f431d8176ec368887030daad1fdbe29c5
Mohab <82895440+Mohab-12@users.noreply.github.com>
2025-06-12 21:31:27 +0300

    Update Streamlit_widget.py

--- Streamlit_widget.py
+++ Streamlit_widget.py
@@ -484,7 +484,7 @@ def main_loop(n, m_frac, T_cout, T_gin, CW_flowrate, steam_flowrate, m_g, a):
     
     # Debug print
     # st.write(f"Segment {i+1}: Gas {T_gin:.1f}→{T_gout:.1f}°C, Water {T_cout:.1f}→{T_c_in:.1f}°C, Wall {T_w:.1f}°C")
-    st.write(f"len Outlet_temp_air: {len(Outlet_temp_air)}, len Inlet_temp_water: {len(Inlet_temp_water)}")
+    # st.write(f"len Outlet_temp_air: {len(Outlet_temp_air)}, len Inlet_temp_water: {len(Inlet_temp_water)}")
 
     
     # Return all the calculated lists
@@ -583,8 +583,8 @@ Reynolds number = {df1.loc[e,'Re']} and
 Mass fraction of water vapour = {df1.loc[e,'Mass Fraction']} %
 """
 
-# st.write(f"len x: {len(np.linspace(1, n, n))}, len Outlet_temp_air: {len(results['Outlet_temp_air'])}")
-# st.write(f"len Outlet_temp_air: {len(Outlet_temp_air)}, len Inlet_temp_water: {len(Inlet_temp_water)}")
+st.write(f"len x: {len(np.linspace(1, n, n))}, len Outlet_temp_air: {len(results['Outlet_temp_air'])}")
+st.write(f"len Outlet_temp_air: {len(Outlet_temp_air)}, len Inlet_temp_water: {len(Inlet_temp_water)}")
 
 # Create a bar plot for condensation data
 fig, ax = plt.subplots(figsize=(10, 6))
