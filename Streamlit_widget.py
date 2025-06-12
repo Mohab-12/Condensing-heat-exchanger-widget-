a2525c9f792c0891ad0fdabbb604d4b80ad09758
Mohab <82895440+Mohab-12@users.noreply.github.com>
2025-06-12 21:25:38 +0300

    Update Streamlit_widget.py

--- Streamlit_widget.py
+++ Streamlit_widget.py
@@ -585,28 +585,28 @@ Mass fraction of water vapour = {df1.loc[e,'Mass Fraction']} %
 
 
 # Create a bar plot for condensation data
-# fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots(figsize=(10, 6))
 
-# # Plot Experimental vs Calculated
-# ax.scatter(np.linspace(1,n,8), Flue_gas[:-1], marker='o', label='Humid air Exp')
-# ax.plot(np.linspace(1,n,n), results['Outlet_temp_air'], 
-#         ls='--', label='Humid air Calc', color='orange')
+# Plot Experimental vs Calculated
+ax.scatter(np.linspace(1,n,9), Flue_gas, marker='o', label='Humid air Exp')
+ax.plot(np.linspace(1,n,n), results['Outlet_temp_air'], 
+        ls='--', label='Humid air Calc', color='orange')
 
-# ax.scatter(np.linspace(1,n,8), Cooling_water[:-1], marker='o', label='Cooling water Exp')
-# ax.plot(np.linspace(1,n,n), results['Inlet_temp_water'], 
-#         ls='--', label='Cooling water Calc', color='g')
+ax.scatter(np.linspace(1,n,9), Cooling_water, marker='o', label='Cooling water Exp')
+ax.plot(np.linspace(1,n,n), results['Inlet_temp_water'], 
+        ls='--', label='Cooling water Calc', color='g')
 
-# # ax.scatter(np.linspace(1,n,8), Wall_temperature1, marker='o', label='Wall temp Exp', color='r')
-# # ax.plot(np.linspace(1,n,n), results['Wall_temperature2'], 
-# #         ls='--', label='Wall temp Calc', color='black')
+# ax.scatter(np.linspace(1,n,9), Wall_temperature1, marker='o', label='Wall temp Exp', color='r')
+# ax.plot(np.linspace(1,n,n), results['Wall_temperature2'], 
+#         ls='--', label='Wall temp Calc', color='black')
 
-# ax.set_xlabel("Local point (row no.)")
-# ax.set_ylabel("Temperature (°C)")
-# ax.legend(bbox_to_anchor=(1.05, 0.65), loc='center left')
-# ax.grid(True)
-# plt.tight_layout()
-# #Display the figure in Streamlit
-# st.pyplot(fig)
+ax.set_xlabel("Local point (row no.)")
+ax.set_ylabel("Temperature (°C)")
+ax.legend(bbox_to_anchor=(1.05, 0.65), loc='center left')
+ax.grid(True)
+plt.tight_layout()
+#Display the figure in Streamlit
+st.pyplot(fig)
 
 # Display experiment parameters below the plots
 st.text(experiment_parameters)
