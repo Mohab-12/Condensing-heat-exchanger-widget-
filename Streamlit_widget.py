eaf23931f04d062468c6ae889ce53c679e71195a
Mohab <82895440+Mohab-12@users.noreply.github.com>
2025-06-12 18:01:23 +0300

    Update Streamlit_widget.py

--- Streamlit_widget.py
+++ Streamlit_widget.py
@@ -292,9 +292,7 @@ def main_loop(n, m_frac, T_cout, T_gin, CW_flowrate, steam_flowrate, m_g, a):
         # 3. Handle temperatures
         if i == 0:
             st.write(f"i:{i}")
-            st.write(f"i:{i}")
-            st.wtite(f"T_cin {T_cin}")
-
+            st.wtite(f"T_cout {T_cout}")
             T_c = T_cout
             T_g = T_gin
         else:
